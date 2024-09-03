8cebc4e048e8ad37483fe5e24bbb7d667f1474b9
Nguyễn Minh Quang <quangnm@905.vn>
2024-09-03 22:40:34 +0700

    Add build storage

---
+++ build-storage.sh
@@ -0,0 +1,7 @@
+kubectl apply -f k8s-storage-rox.yaml
+
+kubectl apply -f k8s-storage-rwo.yaml
+
+kubectl apply -f k8s-storage-rwop.yaml
+
+kubectl apply -f k8s-storage-rwx.yaml
---
+++ clear-storage.sh
@@ -0,0 +1,7 @@
+kubectl delete -f k8s-storage-rox.yaml
+
+kubectl delete -f k8s-storage-rwo.yaml
+
+kubectl delete -f k8s-storage-rwop.yaml
+
+kubectl delete -f k8s-storage-rwx.yaml
